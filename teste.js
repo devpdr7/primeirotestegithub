12e07b66412b34b2f4a4564d9b2bf9fde44e9f25
devpdr7 <junxd62@gmail.com>
2025-03-12 21:10:39 -0300

    Outra branch

--- teste.js
+++ teste.js
@@ -1,30 +1,9 @@
-class Nota {
-    constructor(descricao) {
-        this.descricao = descricao;
-    }
-    exibir() {
-        console.log(this.descricao);
-        console.log('------------');
-    }
-}
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-class Gerenciador {
-    lista = new Array();
-    
-    adicionar(descricao) {
-        let nota = new Nota(descricao);
-        notas.push(nota);
-    }
-
-    exibirTodas() {
-       for(let nota in notas) {
-        nota.exibir();
-       }
-    }
-}
-
-let gerenciador = new Gerenciador();
-
-while(true) {
-    console.log(`1. Adicionar Notas`);
-}
+rl.question("Qual o seu nome?", (nome) => {
+    console.log(`Olá ${nome}`);
+})
